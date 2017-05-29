fc3a4e94e15d7b0448f50cee4d2a61dfcd1970b5
GreeceArtemis <greece.rapu@gmail.com>
2017-05-29 17:38:52 -0500

    map mobile/desktop

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,49 @@
 $('#collapseNav').collapse({
   toggle: false
 })
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -12.1193539, lng: -77.0345899},
+    scrollwheel: false,
+    zoom: 3
+  });
+
+  var infoWindow = new google.maps.Marker({map: map});
+
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+          zoom:10
+        };
+
+        infoWindow.setPosition(pos);
+        map.setCenter(pos);
+        map.setZoom(15);
+      }, function() {
+        errorBrowser(true, Marker, map.getCenter());
+
+      });
+    } else {
+      // En caso no encuentre
+      errorBrowser(false, infoWindow, map.getCenter());
+    }
+  }
+
+  function errorBrowser(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                          'El Navegador no puede ubicarlo' :
+                          'Tu navegador no tinee soporte para ubicarte');
+
+
+  var Partida = document.getElementById("partida");
+  var Destino = document.getElementById("destino");
+  new google.maps.places.Autocomplete(Partida);
+  new google.maps.places.Autocomplete(Destino);
+
+
+
+}
